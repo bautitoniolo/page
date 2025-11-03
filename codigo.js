e7189312d22f5a6ef91f103fcf166c87ad60d26a
bauti <>
2025-11-03 09:44:39 -0300

    created root folder for deploy

---
+++ codigo.js
@@ -0,0 +1,54 @@
+<script>
+function aleatorio(min, max){
+    return Math.floor(Math.random()*(max-min+1) + min)
+}
+
+function eleccion(jugador) {
+    let resultado = ""
+    if(jugador == 1) {
+        resultado = "piedra"
+    } else if(jugador == 2) {
+        resultado = "papel"
+    } else if(jugador == 3){
+        resultado = "tijeras"
+    } else {
+        resultado = "mal elegido"
+    }
+    return resultado  
+}
+// 1 es piedra, 2 es papel, 3 es tijera 
+let jugador = 0 
+let pc = 0
+let triunfos = 0 
+let perdidas = 0
+
+    while (triunfos < 3 && perdidas < 3 ) {
+       pc = aleatorio(1, 3)
+    jugador = prompt("elige: 1 para piedra, 2 para papel, 3 para tijera")
+    //alert("elegiste" + jugador)
+
+     alert ("pc elige: " + eleccion(pc))
+     alert ("tu eliges: " + eleccion(jugador))
+
+     //combate 
+     if(pc == jugador){
+         alert("empate")
+    } else if(jugador == 1 && pc == 3) {
+         alert("ganaste")
+         triunfos = trinunfos + 1
+    }else if(jugador == 2 && pc == 1) {
+         alert("ganaste")
+         triunfos = triunfos + 1 
+    } else if(jugador == 3 && pc == 1) {
+         alert("ganaste")
+         triunfos = triunfos + 1 
+    } else {
+         alert("perdiste")
+         perdidas = perdidas + 1 
+    }
+}
+
+alert("ganaste " + triunfos + "veces. Perdiste" + perdidas + " veces.") 
+
+
+</script>
